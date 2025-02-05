7fd716cda906c5d638f3c287c8e707b8589a3c78
Gabriel Brambila <biel300816@gmail.com>
2025-02-05 16:01:38 -0300

    ultima alteração na calculadora java

--- 05-02/calc.js
+++ 05-02/calc.js
@@ -1,12 +1,25 @@
 //duas variavies
 //num1 e num2
 //perguntar para o usuario
+
+//soma
 let num1 = Number(prompt("Digite um numero"));
 let num2 = Number(prompt("digite outro numero"));
 
 console.log(typeof num1)
 console.log(typeof num2)
 
+
+//operações
+let sub = num1 + num2; 
+
 //imprimir resposta 
 
-console.log(`A soma é ${num1 + num2}`);
+console.log(`A sub é ${num1 - num2}`);
+
+
+
+//sub : -
+//multi: *
+//div: /
+
