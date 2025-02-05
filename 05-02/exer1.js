9c9666aec29c892ae665c0e6bb6c9f39e1898e3a
Gabriel Brambila <biel300816@gmail.com>
2025-02-05 16:08:47 -0300

    peinmeiro exercicio

---
+++ 05-02/exer1.js
@@ -0,0 +1,10 @@
+/*
+
+-Armazene a palavra 'tom' em uma let "Tom";
+-Armazene a palavra 'hanks' em uma let "firstTom";
+-armazene a palavra 'cruise' em uma let "secondTom";
+-declare uma let "myFavoriteTom" que recebe nome, espaço, e sobrenome do
+Tom que você mais gosta;
+-exiba a "myFavoriteTom" no console do browser
+
+*/
