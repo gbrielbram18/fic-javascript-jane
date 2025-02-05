653aab2fd22dad250a3f409a35bef10c20ddcf28
Gabriel Brambila <biel300816@gmail.com>
2025-02-05 15:25:49 -0300

    java

--- 05-02/script.js
+++ 05-02/script.js
@@ -1,10 +1,10 @@
 //variavel
 let nome = prompt("digite seu nome");
 let idade = prompt("digite sua idade");
-let time = prompt("Digite seu time:")
+let time = prompt("Digite seu time:");
 
 //imprimir resposta 
 document.write("seja bem vindo " + nome + ". vc tem: " + idade  + " Anos");
 document.write("<br><br>");
-document.write(`seja bem vindo: ${nome}, sua idade e: ${idade} anos.e vc torce para para ${time}`);
+document.write(`seja bem vindo: ${nome}, sua idade e: ${idade} anos. e vc torce para para ${time}`);
 
