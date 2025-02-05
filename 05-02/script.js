ede38954598f4c738ad6028203336441d3ae5522
Gabriel Brambila <biel300816@gmail.com>
2025-02-05 14:51:00 -0300

    arquivo java

---
+++ 05-02/script.js
@@ -0,0 +1,8 @@
+//variavel
+let nome = prompt("digite seu nome");
+let idade = prompt("digite sua idade");
+
+//imprimir resposta 
+document.write("seja bem vindo " + nome + ". vc tem: " + idade  + " gabiAnos");
+document.write("<br><br>");
+document.write(`seja bem vindo: ${nome}, sua idade e: ${idade} anos.`);
