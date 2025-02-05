3c5f9e9865c5eac24f296e598ae6323d8f545c19
Gabriel Brambila <biel300816@gmail.com>
2025-02-05 14:56:56 -0300

    arquivo primeiros passos em java

--- 05-02/script.js
+++ 05-02/script.js
@@ -1,8 +1,10 @@
 //variavel
 let nome = prompt("digite seu nome");
 let idade = prompt("digite sua idade");
+let time = prompt("Digite seu time:")
 
 //imprimir resposta 
-document.write("seja bem vindo " + nome + ". vc tem: " + idade  + " gabiAnos");
+document.write("seja bem vindo " + nome + ". vc tem: " + idade  + " Anos");
 document.write("<br><br>");
-document.write(`seja bem vindo: ${nome}, sua idade e: ${idade} anos.`);
+document.write(`seja bem vindo: ${nome}, sua idade e: ${idade} anos.e vc torce para para ${time}`);
+
