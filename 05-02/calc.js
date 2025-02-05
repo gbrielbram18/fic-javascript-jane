707f5ece961fda5dbd0d040882fca9f2e9dbf344
Gabriel Brambila <biel300816@gmail.com>
2025-02-05 15:46:15 -0300

    calculadora em java

---
+++ 05-02/calc.js
@@ -0,0 +1,12 @@
+//duas variavies
+//num1 e num2
+//perguntar para o usuario
+let num1 = Number(prompt("Digite um numero"));
+let num2 = Number(prompt("digite outro numero"));
+
+console.log(typeof num1)
+console.log(typeof num2)
+
+//imprimir resposta 
+
+console.log(`A soma é ${num1 + num2}`);
