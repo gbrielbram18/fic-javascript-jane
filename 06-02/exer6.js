d6c5ff2e7736fbc2b4bd63cb22cbe10823f789b5
Gabriel Brambila <biel300816@gmail.com>
2025-02-06 15:59:33 -0300

    adicionando atividade 6

---
+++ 06-02/exer6.js
@@ -0,0 +1,7 @@
+const summerMessage = "O VERÃO CHEGA NESTE FINAL DE SEMANA"
+const summerEnd = "o verão acabará este sabado"
+
+console.log(summerMessage.toLowerCase());
+
+//toUpperCase()
+console.log(summerEnd.toUpperCase()); 
