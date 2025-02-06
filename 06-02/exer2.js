0e45772431558c6a75c0840c3f16135e6d9ca179
Gabriel Brambila <biel300816@gmail.com>
2025-02-06 14:50:13 -0300

    adicioando exercicio 2

---
+++ 06-02/exer2.js
@@ -0,0 +1,19 @@
+//string
+let time = "CORINTHIANS"
+
+//inteiro
+let data = "06/02"
+
+//booleano
+let solteiro = true
+
+//Numeros flutuantes com casas decimais
+let valorPassagem = "5.30"
+
+
+//imprimir todas as variaveis
+
+console.log("meu time e: " + time)
+console.log("data de hoje é :" + data)
+console.log("casado?" + solteiro)
+console.log("preço da passagem de onibus R$" + valorPassagem)
