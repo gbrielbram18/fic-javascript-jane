f752d723acd31bedd7c2d811e13c5c80ebd6f7d0
Gabriel Brambila <biel300816@gmail.com>
2025-02-06 14:38:08 -0300

    adicionando tipos de dados

---
+++ 06-02/tipos-dados.js
@@ -0,0 +1,17 @@
+//string
+let nome = "Gabriel";
+
+//inteiro
+let idade = 18;
+
+//Booleano
+let casado = false; 
+
+// Números flutuantes com casas decimais
+let valorProduto = 5.32;
+
+
+console.log("Meu nome e:" + nome);
+console.log("idade" , idade);
+console.log("casado?" + casado);
+console.log("R$" , valorProduto);
