9e1e5e12487c27c5d12f525571cf38e3c1fd5957
Gabriel Brambila <biel300816@gmail.com>
2025-02-06 14:27:21 -0300

    adicionando exemplos de variavel

---
+++ 06-02/variavel.js
@@ -0,0 +1,13 @@
+let mensagen = "Olá mundo";
+const nomeCompleto = "ola Gabriel Lima Brambila"
+
+console.log (mensagen `seja bem vindo`)
+//idade
+let idade = 18
+
+idade = 38
+
+console.log( nomeCompleto)
+console.log(`${mensagem} e a idade é ${idade}`)
+
+//let uma constante e nao pode ser alterada 
