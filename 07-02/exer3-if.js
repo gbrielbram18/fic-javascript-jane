3f1f98082b3bb74f1914f69a42e5d022a5632af8
Gabriel Brambila <biel300816@gmail.com>
2025-02-07 16:11:57 -0300

    adicionando atividade if

---
+++ 07-02/exer3-if.js
@@ -0,0 +1,11 @@
+/*
+    se a idade >= 18, a pessoa pode ir para festa,
+    senão, a pessoa noa pode entrar na festa
+*/ 
+let idade = 18;
+
+if (idade>=18){
+console.log("Voce nao pode ir a festa pois vc e menor de idade")
+}else{
+    console.log("voce pode entrar na festa pois ja e maior de idade ")
+}
--- 07-02/if.js
+++ 07-02/if.js
@@ -2,5 +2,7 @@ let valor = 7;
 
 if (valor>=7) {
  console.log(" o valor e verdadeiro")
+} else {
+    console.log("o valor e falso")
 }
 
