af6e4874091c7f8395aff20d25863091189ba56c
Gabriel Brambila <biel300816@gmail.com>
2025-02-07 14:51:34 -0300

    adicionando exercicio

---
+++ 07-02/array.js
@@ -0,0 +1,6 @@
+const nomes = [];
+
+nomes.push("hugo neneca" ,"yuri alberto", "garro")
+
+//imprimir nomes
+console.log(nomes)
---
+++ 07-02/exer1.js
@@ -0,0 +1,13 @@
+/*
+    use o metodo adequado para adicionar os 3 seguntes itens
+
+    -coxinha
+    -null
+    -brigadeiro
+
+*/
+
+let brazilianFood = [];
+
+
+console.log()
