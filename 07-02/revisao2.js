6fa2e264c0689e8d8501ee81b3af3f07ac34d981
Gabriel Brambila <biel300816@gmail.com>
2025-02-07 14:36:45 -0300

    revisao2

---
+++ 07-02/revisao2.js
@@ -0,0 +1,6 @@
+let text=  "estao  chegando as provas";
+
+//retorna a primeira possição
+console.log(text.indexOf("as"));
+//retorna a ultima posição na string
+console.log(text.lastIndexOf("as"));
