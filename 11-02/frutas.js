6fb78f90801f8baa2e3e588cfd7b417bcf04dca7
Gabriel Brambila <biel300816@gmail.com>
2025-02-11 15:00:02 -0300

    adicionando verificador de fruta

---
+++ 11-02/frutas.js
@@ -0,0 +1,13 @@
+/* 
+    -abaixo do array "fruits", verifique se "abacaxi" existe no array. se 
+    existir, exiba no console a mensagem "a string "abacaxi" exsite no array
+    fruits."
+
+    -se "abacaxi" nao existir, verifique se "pera" existe no array. se existir, exiba 
+    no console a mensagem " a string "pera" existe no array fruits."
+
+    -se nenhuma das duas frutas exisitrem no array, rxiba no console a mensagem
+    "nem pera nem abacaxi existem no array "fruits"."
+    
+*/
+
