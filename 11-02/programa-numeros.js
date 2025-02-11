f5855e3194fc7aa131c5e42f8984bdfb61ff1274
Gabriel Brambila <biel300816@gmail.com>
2025-02-11 14:23:10 -0300

    adicionando js do index

--- 11-02/programa-numeros.js
+++ 11-02/programa-numeros.js
@@ -4,3 +4,13 @@
     se o numero for maior que zero e possitivo
     Senão o numero não e valido
 */ 
+
+
+let numero = -5
+
+if ( numero >= 0 ) {
+    console.log(" o numero e positivo")
+
+}else if (numero <=0 ) {
+    console.log( "o numero e negativo")
+}
