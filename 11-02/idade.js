159148b48c8bf245db227343a4a30c748afc55df
Gabriel Brambila <biel300816@gmail.com>
2025-02-11 14:13:03 -0300

    adicionando html e java do html

---
+++ 11-02/idade.js
@@ -0,0 +1,12 @@
+let idade = 12
+
+if ( idade <= 12) {
+    console.log(" w uma criança")
+
+}else if (idade <=17 ) {
+    console.log( "e um adolecente")
+} else if ( idade>= 18) {
+    console.log(" ele e um jovem")
+} else {
+    console.log (" e um adulto")
+}
---
+++ 11-02/prgrema-numeros.js
@@ -0,0 +1,6 @@
+/*
+    - verifica se o numero e possitivo ou negativo ou zero
+    se o numero for menor que o zero , é negativo
+    se o numero for maior que zero e possitivo
+    Senão o numero não e valido
+*/ 
