4eaa871fb528a19a93591f0336ab7ae3b2548c93
Gabriel Brambila <biel300816@gmail.com>
2025-02-11 13:52:45 -0300

    arquivo explicação if alterado

--- 11-02/if.js
+++ 11-02/if.js
@@ -4,7 +4,8 @@ se a idade >= 18,  a pessoa pode ir para festa, senão a pessoa nao pode ir a fe
 
 let idade = 18;
 
-if (18) {
-    
-
+if (idade >= 18) {
+    console.log (" a pessoa, pode ir a festa")
+} else{
+    console.log(" a pessoa nao pode ir a festa ")
 }
