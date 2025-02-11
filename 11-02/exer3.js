f181404904664abe52700092fa68790df98c0aa6
Gabriel Brambila <biel300816@gmail.com>
2025-02-11 16:17:42 -0300

    adicionando exercicio

---
+++ 11-02/exer3.js
@@ -0,0 +1,17 @@
+/*
+    -Armazenar sua idade em uma constante;
+    -escreva uma estrutura condicional em que, se a idade e 7 anos ou menos, se a idade e 65 anos ou mais, a frase " para voce a entrada e gratuita!" e
+    armazenada em uma variavel;
+    -se a idade nao atende as condições acima, a variavel deve armazenar a mensagem "a entrada e R$30,00"
+    -exiba a mensagem no console
+    -agora, teste diferentes idade para se certirficar que a condição do 'if'
+    tambem esta funcionando 
+*/
+
+let idade = prompt("digite sua idade")
+
+ if (idade < 7 && valor < 7 || valor > 65 && idade >65){
+    console.log(" sua entrada e gratis")
+ }else{
+    console.log( "sua entrada ficou R$30,00")
+ }  
