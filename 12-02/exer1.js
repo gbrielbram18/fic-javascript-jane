eaf9c952c2f14f11787e8a680de7d9028d9e4311
Gabriel Brambila <biel300816@gmail.com>
2025-02-12 14:56:04 -0300

    adicionando exercicos e exemplo de for

---
+++ 12-02/exer1.js
@@ -0,0 +1,14 @@
+/* 
+    -escreva um loop (for) que exiba a frase abaixo 
+    no console;
+    - substitua o "x" pela informação correta; 
+    - "x" deve começar e 1 na 1° exibição da frase e em 10 
+    na ultima;
+    - exiba a frase 10X no console
+
+    "esta e a X vez que esta frase e exibida no console"
+    */
+
+    for (let i = 1; i <= 10; i ++){
+    console.log(`esta e a ${i}ª vez que esta frase e exibida no console.`)       
+    }
---
+++ 12-02/exer2.js
@@ -0,0 +1,18 @@
+/*
+    -abaixo da constanre "cities", declara uma let "upperCaseCities" e atribua a ela um array vazio;
+    -adicionar no array que a "upperCaseCities" armazena as cidades do array cities com todas  as letras maiusculas, utilizando o for
+    loop.
+    -para ver se deu certo, exiba o "upperCaseCities" no console.
+    push() - dica 
+    [] array vazio - dica
+*/
+
+const cities = [`sao paulo`, `rio de janeiro`,  `minas gerais`]
+let upperCaseCities = []
+console.log(cities.length)
+
+for ( let i = 0 ; i< cities.length; i++){
+
+}
+
+console.log(upperCaseCities)
