653fd78e456680cbf41c6600230eb4482e66601a
Gabriel Brambila <biel300816@gmail.com>
2025-02-14 15:53:31 -0300

    modificando js e html

---
+++ 14-02/js/nota.js
@@ -0,0 +1,9 @@
+
+//variaveis
+const inputNota = document.getElementById("nota")
+const resultado = document.getElementById("resultado")
+
+function resultadoNotaCLick(){
+    console.log("Ola gabriel")
+    //alert("ola gabriel")
+}
