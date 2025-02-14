75a4e80ca2851977f04400ac276d00cacc0e1e2e
Gabriel Brambila <biel300816@gmail.com>
2025-02-14 16:16:59 -0300

    adicionando exercicio js

--- 14-02/js/nota.js
+++ 14-02/js/nota.js
@@ -4,6 +4,16 @@ const inputNota = document.getElementById("nota")
 const resultado = document.getElementById("resultado")
 
 function resultadoNotaCLick(){
-    console.log("Ola gabriel")
-    //alert("ola gabriel")
+    //previne um comportamento no navegador
+    event.preventDefault()
+    console.log(inputNota.value)
+  
+if (inputNota.value >=6 ){
+    resultado.innerHTML = "aprovado"
+}else {
+    resultado.innerHTML = "reprovado"
+}
+
+
+inputNota.value = ""
 }
