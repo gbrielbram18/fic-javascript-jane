2da79bd787190073b273bb85c468a3f2e4b82c1d
Gabriel Brambila <biel300816@gmail.com>
2025-02-18 14:31:32 -0300

    adicionando exercicio modificado

--- 14-02/js/nota.js
+++ 14-02/js/nota.js
@@ -2,18 +2,33 @@
 //variaveis
 const inputNota = document.getElementById("nota")
 const resultado = document.getElementById("resultado")
+const mensage = document.getElementById("mes")
 
-function resultadoNotaCLick(){
+function resultadoNotaCLick(event) {
     //previne um comportamento no navegador
     event.preventDefault()
     console.log(inputNota.value)
-  
-if (inputNota.value >=6 ){
-    resultado.innerHTML = "aprovado"
-}else {
-    resultado.innerHTML = "reprovado"
+
+
+    //validação de formulario
+    if (inputNota.value === "") {
+        mensage.style.display = "block" 
+        return false
+    }
+
+    //condição
+    if (inputNota.value >= 6) {
+        resultado.innerHTML = "aprovado"
+    } else {
+        resultado.innerHTML = "Reprovado"
+    }
+
+    mensage.style.display = "none"
+
+
+
+    inputNota.value = ""
+
 }
 
 
-inputNota.value = ""
-}
