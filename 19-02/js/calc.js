b7e2ccbe6a2b4a4900866bae30912e90c9e60506
Gabriel Brambila <biel300816@gmail.com>
2025-02-19 16:10:26 -0300

    adicionando exercicio

---
+++ 19-02/js/calc.js
@@ -0,0 +1,61 @@
+//obtem os elementos do DOM
+const valor1 =document.getElementById("valor1")
+const valor2 =document.getElementById("valor2")
+
+//variavel
+
+
+
+//função para executar a const
+function somar(event){
+    event.preventDefault();
+    console.log(valor1.value);
+    console.log(valor2.value);
+
+
+//operação
+
+let resultadoSoma= Number(valor1.value) + Number(valor2.value)
+
+//imprimindo reposta
+
+document.getElementById("resultado").innerHTML = resultadoSoma
+
+
+}
+   
+
+
+
+//função para sub
+function sub(event){
+    event.preventDefault();
+    console.log("sub")
+
+
+    console.log(valor1.value)
+    console.log(valor2.value)
+
+
+    let resultadoSub = Number(valor1.value) - Number(valor2.value)
+
+    document.getElementById("resultado").innerHTML = resultadoSub
+
+}
+
+
+
+
+function divsao(event){
+    event.preventDefault();
+    console.log("divisao")
+
+
+    console.log(valor1.value)
+    console.log(valor2.value)
+
+
+    let resultadoSub = Number(valor1.value) - Number(valor2.value)
+
+    document.getElementById("resultado").innerHTML = resultadoSub
+}
