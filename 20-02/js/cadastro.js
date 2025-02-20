83409963f61cd139bc9e2371ed6443ad59dca6b9
Gabriel Brambila <biel300816@gmail.com>
2025-02-20 14:42:06 -0300

    adicionando exercicos de cadastros

---
+++ 20-02/js/cadastro.js
@@ -0,0 +1,27 @@
+const campo = document.querySelector("#campo")
+const salvarBtn = document.querySelector("#salvar")
+
+//adicionar um evento no botao salvar 
+salvarBtn.addEventListener("click" , function(){
+const valor = campo.value;
+   
+
+// salvar no locaStorage
+
+    const chave = `ValorSalvo_${Date.now()}`;  //usa timestamp para a chave unica
+
+ //funcionalidade do localStorage
+    localStorage.setItem(chave , valor)
+
+
+
+
+
+});
+
+
+//limpar o campo apos salvar
+
+campo.value = ""
+
+
