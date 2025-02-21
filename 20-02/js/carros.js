acfaf6ba1963e38b63d492e66c5d98faee10f9b5
Gabriel Brambila <biel300816@gmail.com>
2025-02-21 15:00:25 -0300

    adicionando exercicios modificados

--- 20-02/js/carros.js
+++ 20-02/js/carros.js
@@ -8,9 +8,37 @@ const cars = [
         brand : "Honda",
         model : "Civic",
         year : 2008
-    }
+    },
+    {
+        brand: "opala",
+        model :"woksvagem",
+        year: 2006
+    },
+    {
+        brand:"Mustang",
+        model:"Ford",
+        year: 2010
+    },
+    {
+        brand:"Porsche",
+        model:"Porsche 911",
+        year: 2025
+    },
+
+
+
+
 ]
 
 //mostra a informação pedida no console.log
-console.log(cars[1].year)// cars[1].year e o que pede a informação e o console a que passa para o site
-console.log(cars[0].year)
+//console.log(cars[1].year)// cars[1].year e o que pede a informação e o console a que passa para o site
+//console.log(cars[0].year)
+
+
+/*criar uma função e chama essa função displayCard, aqui mesmo*/
+
+function displayCard(){
+    console.log("displayCard")
+};
+
+//Chamar função
