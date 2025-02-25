506e941f806a759df5952f672b0467a0e7d23286
Gabriel Brambila <biel300816@gmail.com>
2025-02-25 15:57:09 -0300

    adicionando arquivos react

--- react/primeiro-projeto/src/App.jsx
+++ react/primeiro-projeto/src/App.jsx
@@ -5,9 +5,21 @@ function App() {
 
   return (
     <>
-    <p>
-      gabriel
-    </p>
+    <div className="container">
+      <header>
+        <h1>Logo</h1>
+      </header>
+      
+      <h1>seja bem vindo</h1>
+
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Fuga eos corporis ducimus odit, expedita ab quibusdam saepe, iusto placeat facere, harum dolorem cupiditate distinctio sunt repudiandae nihil deserunt at.
+          Nemo excepturi laboriosam, sed sit est provident ea nisi debitis assumenda aperiam impedit? Praesentium explicabo tempora id magnam voluptatum quasi inventore?
+      </p>
+
+    </div>
 
     </>
   )
